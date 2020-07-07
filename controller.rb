75f8cf7ef07d34c658a08271733e0f49225bddf4
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-07-07 17:57:30 +0100

    refactored films list to move mapping logic to controller rather than index

--- controller.rb
+++ controller.rb
@@ -5,6 +5,7 @@ require_relative "models/film"
 also_reload "models/*"
 
 get "/" do
-    @films = Film.all
+    films = Film.all
+    @films_display = films.map { |film| "#{film.title} : £#{film.price}" }
     erb(:index)
 end
